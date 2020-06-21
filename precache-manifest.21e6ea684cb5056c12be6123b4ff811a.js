34386a42ff8ea4dbe206fc05dadcdb97ab83bec4
imanmehni <imanmehni1991@gmail.com>
2020-06-21 09:10:15 +0430

    Updates

---
+++ precache-manifest.21e6ea684cb5056c12be6123b4ff811a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "305e6441abd79187f529d96699b81db3",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "4b4f953dc3fd7a0d2770",
+    "url": "/todo/static/css/main.90395d80.chunk.css"
+  },
+  {
+    "revision": "a793c7d9b67d52c8a646",
+    "url": "/todo/static/js/2.2f6fe90e.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/todo/static/js/2.2f6fe90e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b4f953dc3fd7a0d2770",
+    "url": "/todo/static/js/main.a6bc75ba.chunk.js"
+  },
+  {
+    "revision": "e6dc4d3a19400df1f5a0",
+    "url": "/todo/static/js/runtime-main.8dd2ff45.js"
+  }
+]);
